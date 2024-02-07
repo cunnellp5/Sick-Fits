dacdd2fe2599fc170166f50f0f8d9a5d98d6bfe4
cunnellp5 <philip.cunnell@colorado.edu>
2024-02-07 13:22:45 -0700

    initialize user in DB

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,36 +1,58 @@
-import "dotenv/config";
-import { config, createSchema } from "@keystone-next/keystone/schema";
+import { User } from './schemas/User';
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
 
 const databaseURL =
   process.env.DATABASE_URL ||
-  "postgres://localhost/keystone-sick-fits-tutorial-main";
+  'postgres://localhost/keystone-sick-fits-tutorial-main';
 
 const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360, // How long they stay signed in
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: Add in inital roles here
   },
-  lists: createSchema({
-    // Schema items go in here
-  }),
-  db: {
-    adapter: "knex",
-    url: databaseURL,
-  },
-  ui: {
-    // Show the UI only for people who pass this test
-    isAccessAllowed: ({ session }) => {
-      // console.log(session);
-      // return !!session?.data;
-      return true;
-    },
-  },
-  // TODO: Add session values here
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    lists: createSchema({
+      // Schema items go in here
+      User,
+    }),
+    db: {
+      adapter: 'knex',
+      url: databaseURL,
+      dropDatabase: true,
+    },
+    ui: {
+      // Show the UI only for people who pass this test
+      isAccessAllowed: ({ session }) => {
+        console.log(session);
+        return !!session?.data;
+      },
+    },
+    // TODO: Add session values here
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id name email',
+    }),
+  })
+);

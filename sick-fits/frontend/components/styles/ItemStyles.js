c4a0b11c0d8b7cc07b3693a069c9866fbbc0b149
cunnellp5 <philip.cunnell@colorado.edu>
2024-02-10 16:33:04 -0700

    sort styles

--- sick-fits/frontend/components/styles/ItemStyles.js
+++ sick-fits/frontend/components/styles/ItemStyles.js
@@ -1,31 +1,31 @@
 import styled from 'styled-components';
 
-const Item = styled.div`
+const ItemStyles = styled.div`
   background: white;
   border: 1px solid var(--offWhite);
   box-shadow: var(--bs);
-  position: relative;
   display: flex;
   flex-direction: column;
+  position: relative;
   img {
-    width: 100%;
     height: 400px;
     object-fit: cover;
+    width: 100%;
   }
   p {
-    line-height: 2;
-    font-weight: 300;
     flex-grow: 1;
-    padding: 0 3rem;
     font-size: 1.5rem;
+    font-weight: 300;
+    line-height: 2;
+    padding: 0 3rem;
   }
   .buttonList {
-    display: grid;
-    width: 100%;
-    border-top: 1px solid var(--lightGray);
-    grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
-    grid-gap: 1px;
     background: var(--lightGray);
+    border-top: 1px solid var(--lightGray);
+    display: grid;
+    grid-gap: 1px;
+    grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+    width: 100%;
     & > * {
       background: white;
       border: 0;
@@ -35,4 +35,4 @@ const Item = styled.div`
   }
 `;
 
-export default Item;
+export default ItemStyles;

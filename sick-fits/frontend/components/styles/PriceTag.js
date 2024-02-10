6542c14a2f20137f451f12f785666fc20bf42c9a
cunnellp5 <philip.cunnell@colorado.edu>
2024-02-10 16:33:11 -0700

    sort styles again

--- sick-fits/frontend/components/styles/PriceTag.js
+++ sick-fits/frontend/components/styles/PriceTag.js
@@ -2,16 +2,16 @@ import styled from 'styled-components';
 
 const PriceTag = styled.span`
   background: var(--red);
-  transform: rotate(3deg);
   color: white;
-  font-weight: 600;
-  padding: 5px;
-  line-height: 1;
-  font-size: 3rem;
   display: inline-block;
+  font-size: 3rem;
+  font-weight: 600;
+  line-height: 1;
+  padding: 5px;
   position: absolute;
-  top: -3px;
   right: -3px;
+  top: -3px;
+  transform: rotate(3deg);
 `;
 
 export default PriceTag;

20988a6da4cc48aea5cffc3a9c97f12d770c14bb
cunnellp5 <philip.cunnell@colorado.edu>
2024-02-12 17:17:03 -0700

    single product and dynamic page rendering

---
+++ sick-fits/frontend/components/SingleProduct.js
@@ -0,0 +1,64 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import Head from 'next/head';
+import DisplayError from './ErrorMessage';
+import styled from 'styled-components';
+
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($id: ID!) {
+    Product(where: { id: $id }) {
+      name
+      price
+      description
+      id
+      photo {
+        altText
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+  }
+`;
+
+const ProductStyles = styled.div`
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  max-width: var(--maxWidth);
+  justify-content: center;
+  align-items: top;
+  gap: 2rem;
+  img {
+    width: 100%;
+    object-fit: contain;
+  }
+`;
+
+export default function SingleProduct({ id }) {
+  const { data, error, loading } = useQuery(SINGLE_ITEM_QUERY, {
+    variables: { id },
+  });
+
+  //   this is becoming undefined randomly
+  //   const { Product } = data;
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <DisplayError error={error}></DisplayError>;
+
+  return (
+    <ProductStyles>
+      <Head>
+        <title>Sick Fits | {data.Product.name}</title>
+      </Head>
+      <img
+        src={data.Product.photo.image.publicUrlTransformed}
+        alt={data.Product.name}
+      ></img>
+      <div className="details">
+        <h2>{data.Product.name}</h2>
+        <p>{data.Product.description}</p>
+      </div>
+    </ProductStyles>
+  );
+}

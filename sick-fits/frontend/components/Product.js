d9e96689ef56f3ae3d41af8a9887fc42f1df234e
cunnellp5 <philip.cunnell@colorado.edu>
2024-02-14 13:00:52 -0700

    better type

--- sick-fits/frontend/components/Product.js
+++ sick-fits/frontend/components/Product.js
@@ -13,20 +13,30 @@ export const Product = ({ product }) => (
     </Title>
     <PriceTag>{formatMoney(product.price)}</PriceTag>
     <p>{product.description}</p>
-    {/* todo addd buttons to edit and delete item */}
+
+    <div className="buttonList">
+      <Link
+        href={{
+          pathname: '/update',
+          query: { id: product.id },
+        }}
+      >
+        Edit -
+      </Link>
+    </div>
   </ItemStyles>
 );
 
 Product.propTypes = {
-  product: {
+  product: PropTypes.shape({
     id: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
     description: PropTypes.string.isRequired,
-    photo: {
-      image: {
+    photo: PropTypes.shape({
+      image: PropTypes.shape({
         publicUrlTransformed: PropTypes.string.isRequired,
-      },
-    },
-  },
+      }),
+    }),
+  }),
 };

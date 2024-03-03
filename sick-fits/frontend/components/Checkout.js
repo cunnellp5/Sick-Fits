f80553244d9c5e2ac9072f8a91a315c52273c7b6
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-03 12:40:39 -0700

    Update checkout

--- sick-fits/frontend/components/Checkout.js
+++ sick-fits/frontend/components/Checkout.js
@@ -1,4 +1,8 @@
+/* eslint-disable react/jsx-no-bind */
 import styled from 'styled-components';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements, CardElement } from '@stripe/react-stripe-js';
+import SickButton from './styles/SickButton';
 
 const CheckoutFromStyles = styled.div`
   box-shadow: 0 1px 2px 2px rgba(0, 0, 0, 0.04);
@@ -9,10 +13,20 @@ const CheckoutFromStyles = styled.div`
   grid-gap: 1rem;
 `;
 
+const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
+
 export default function Checkout() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('wip');
+  }
+
   return (
-    <CheckoutFromStyles>
-      <p>hey</p>
-    </CheckoutFromStyles>
+    <Elements stripe={stripeLib}>
+      <CheckoutFromStyles onSubmit={handleSubmit}>
+        <CardElement />
+        <SickButton>Check out now</SickButton>
+      </CheckoutFromStyles>
+    </Elements>
   );
 }

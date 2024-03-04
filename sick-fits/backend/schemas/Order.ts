c7c09bb95d51e5024a8fd5862f098349281e71d8
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-04 11:47:14 -0700

    order and orderitems

---
+++ sick-fits/backend/schemas/Order.ts
@@ -0,0 +1,17 @@
+import { integer, relationship, text, virtual } from '@keystone-next/fields';
+import { list } from '@keystone-next/keystone/schema';
+import formatMoney from '../lib/formatMoney';
+
+export const Order = list({
+  fields: {
+    label: virtual({
+      graphQLReturnType: 'String',
+      resolver: (item) =>
+        `custom message in order for ${formatMoney(item.total)}`,
+    }),
+    total: integer(),
+    items: relationship({ ref: 'OrderItem.order', many: true }),
+    user: relationship({ ref: 'User.orders' }),
+    charge: text(),
+  },
+});

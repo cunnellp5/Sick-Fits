7ae52eae60bbefe8c02cc968d15d9a9fe28d3a0d
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-07 08:53:20 -0700

    order page

---
+++ sick-fits/frontend/pages/order/[id].js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+import SingleOrder from '../../components/SingleOrder';
+
+export default function SingleOrderPage({ query }) {
+  return <SingleOrder id={query.id} />;
+}
+
+SingleOrderPage.propTypes = {
+  query: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+  }).isRequired,
+};

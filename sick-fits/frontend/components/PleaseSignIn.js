2ee66f543630acfc035a65425e3fb658df082c08
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-12 13:51:47 -0600

    adding signin component if not signed in

---
+++ sick-fits/frontend/components/PleaseSignIn.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+import SignIn from './SignIn';
+import useUser from './User';
+
+export default function PleaseSignIn({ children }) {
+  const me = useUser();
+
+  if (!me) return <SignIn />;
+
+  return { children };
+}
+
+PleaseSignIn.propTypes = {
+  children: PropTypes.any,
+};
